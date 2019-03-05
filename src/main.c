dc55b766da9493c299b6c43cd304dcb4d548cd95
afiebig <alfredo.fiebig@continuum.cl>
2019-03-05 09:29:15 -0300

    Check if port is selected correctly before write

--- src/main.c
+++ src/main.c
@@ -5,20 +5,20 @@ int main() {
   char* portName = "/dev/cu.wchusbserial14240"; //Adaptador CH340
 
   printf("Selecting Port %s\n", portName);
-  select_port(portName);
-
-  printf("Open and Configure port: %s\r\n", sp_get_port_name(port));
-  int retval = sp_open(port, SP_MODE_WRITE | SP_MODE_READ);
-  if (retval == SP_OK)
-  {
-    puts("Serial port successfully opened.\n");
-
-    if (ConfigureSerialPort(port) == 0)
+  int retval = select_port(portName);
+  if ( retval == SP_OK ){
+    printf("Open and Configure port: %s\r\n", sp_get_port_name(port));
+    retval = sp_open(port, SP_MODE_WRITE | SP_MODE_READ);
+    if (retval == SP_OK)
     {
-      printf("Start Writing...\n\n");
-      Polling();
+      puts("Serial port successfully opened.\n");
+      if (ConfigureSerialPort(port) == SP_OK)
+      {
+        printf("Start Writing...\n\n");
+        Polling();
+      }
+      closePort();
+      return 0;
     }
-    closePort();
-    return 0;
   }
 }
