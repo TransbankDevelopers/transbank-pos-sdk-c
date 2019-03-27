2eaf68b2214713699dd6ccc125aba0de6c3577d5
afiebig <alfredo.fiebig@continuum.cl>
2019-03-27 13:05:30 -0300

    Add example for load_keys

---
+++ examples/load_keys.c
@@ -0,0 +1,28 @@
+#include "transbank.h"
+#include "transbank_serial_utils.h"
+
+int main() {
+
+  char* portName = "COM4";
+  int retval = open_port(portName, 115200);
+  if ( retval == TBK_OK ){
+    puts("Serial port successfully opened.\n");
+
+    LoadKeyCloseResponse rsp = load_keys();
+    printf("MAIN:\n");
+    printf("Function: %i\n", rsp.function);
+    printf("Response Code: %i\n", rsp.responseCode);
+    printf("Commerce Code: %llu\n", rsp.commerceCode);
+    printf("Terminal ID: %lu\n", rsp.terminalId);
+    puts("Keys loaded sucsesfully\n");
+  }
+  //Close Port
+  retval = close_port();
+  if(retval == SP_OK){
+    puts("Serial port closed.\n");
+  } else{
+    puts("Unable to close serial port.\n");
+  }
+
+  return 0;
+}
