94c0b02df4430a314743ba9e7ac5550e52bf8782
afiebig <alfredo.fiebig@continuum.cl>
2019-04-04 10:16:05 -0300

    Add Register Close method
    Also rename parse method to parse LoadKeys and Close Responses

--- src/transbank.h
+++ src/transbank.h
@@ -15,6 +15,7 @@ enum TbkReturn{
 };
 
 extern enum TbkReturn open_port(char* portName, int baudrate);
+extern LoadKeyCloseResponse register_close();
 extern LoadKeyCloseResponse load_keys();
 extern enum TbkReturn polling();
 extern enum TbkReturn set_normal_mode();
