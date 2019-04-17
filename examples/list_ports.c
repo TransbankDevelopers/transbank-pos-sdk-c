00e38753bcc748a458f0ebf1eb7c2d536017b5a7
Leonel Muñoz <leonel.munoz@continuum.cl>
2019-04-17 17:50:30 -0400

    Separate example for listing ports

---
+++ examples/list_ports.c
@@ -0,0 +1,20 @@
+#include "transbank.h"
+#include "transbank_serial_utils.h"
+
+int main()
+{
+  // Example, listing available ports
+  char *list = list_ports();
+
+  const char *delim = "|";
+  char *ptr = strtok(list, delim);
+
+  printf("Available ports : \n");
+  while (ptr != NULL)
+  {
+    printf("-> %s\n", ptr);
+    ptr = strtok(NULL, delim);
+  }
+
+  return 0;
+}
