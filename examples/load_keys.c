dd31560dba4c543cb234e38cb202d393fc29ef8e
Leonel Muñoz <leonel.munoz@continuum.cl>
2019-04-18 18:02:15 -0400

    Improve logics

--- examples/load_keys.c
+++ examples/load_keys.c
@@ -5,68 +5,85 @@ int main()
 {
   // VELOCIDAD COM
   const int bRate = 115200;
+  const char sep = '|';
 
   // Getting available ports
-  char *list = list_ports();
-  const char *delim = "|";
-  char *ptr = strtok(list, delim);
+  char *list = strcat(list_ports(), "|"); // Add pipe for examples
+
+  int x, count = 0;
+  for (x = 0; x <= strlen(list); x++)
+  {
+    if (list[x] == sep)
+    {
+      count++;
+    }
+  }
+
+  char **ports = malloc(count * sizeof(char *));
+  char *w;
+  int i, l, f = 0;
 
   printf("Available ports : \n=================\n");
-  int count = 1;
-  while (ptr != NULL)
+  for (x = 0; x <= strlen(list); x++)
   {
-    printf("%i) %s\n", count, ptr);
-    ptr = strtok(NULL, delim);
-    count++;
+    if (list[x] == sep)
+    {
+      w = malloc(l * sizeof(char *));
+      strncpy(w, list + i, l);
+      w[l] = 0;
+
+      printf("%i) %s\n", (f + 1), w); // Display option
+
+      // Store port (w) in array list
+      ports[f] = malloc(sizeof(w));
+      strcpy(ports[f], w);
+
+      f++;
+      i = x + 1; // Set new position
+      l = 0;
+      continue;
+    }
+
+    l++;
   }
 
-  // Populate array list
-  char **ports = malloc(count * sizeof(char *));
-  ptr = strtok(list, delim);
-  int n = 0;
-  while (ptr != NULL)
-  {
-    strcpy(ports[n], ptr);
-    ptr = strtok(NULL, delim);
-    n++;
-  }
-
-  // Handle option
+  // Handle input option
   printf("Please insert port option:");
   int op;
   scanf("%i", &op);
 
   char *portName = ports[op - 1];
-  printf("Using \"%s\" port ...\n=================\n", portName);
-  free(ports);
+  printf("Using option: %i -> \"%s\" port ...\n=================\n", op, portName);
 
   int resp = open_port(portName, bRate);
-  if (resp != TBK_OK)
+  if (resp == TBK_OK)
+  {
+    printf("Serial port successfully opened...\n");
+
+    LoadKeyCloseResponse lcr = load_keys();
+
+    // TODO(lm): Check if response is correct :/
+    printf("Function: %i\n", lcr.function);
+    printf("Response Code: %i\n", lcr.responseCode);
+    printf("Commerce Code: %llu\n", lcr.commerceCode);
+    printf("Terminal ID: %lu\n", lcr.terminalId);
+    puts("Keys loaded successfully.\n=================\n");
+
+    //Close Port
+    resp = close_port();
+    if (resp == SP_OK)
+    {
+      printf("Serial port closed successfully.\n"); // Success!
+    }
+    else
+    {
+      puts("Unable to close serial port.\n");
+    }
+  }
+  else
   {
     puts("Unable to open serial port.\n");
-    return 0;
   }
 
-  printf("Serial port successfully opened...\n");
-
-  LoadKeyCloseResponse lcr = load_keys();
-
-  // TODO(lm): Check if response is correct :/
-  printf("Function: %i\n", lcr.function);
-  printf("Response Code: %i\n", lcr.responseCode);
-  printf("Commerce Code: %llu\n", lcr.commerceCode);
-  printf("Terminal ID: %lu\n", lcr.terminalId);
-  puts("Keys loaded successfully.\n=================\n");
-
-  //Close Port
-  resp = close_port();
-  if (resp != SP_OK)
-  {
-    puts("Unable to close serial port.\n");
-    return 0;
-  }
-
-  // Success!
-  printf("Serial port closed successfully.\n");
   return 0;
 }
