71d00e756e2d120c81b7f1a2c102297c806ac28d
Leonel Muñoz <leonel.munoz@continuum.cl>
2019-04-29 14:56:12 -0400

    Add interactive option to sale example

--- examples/sale.c
+++ examples/sale.c
@@ -1,25 +1,81 @@
 #include "transbank.h"
 #include "transbank_serial_utils.h"
 
-int main() {
+int main()
+{
+  // VELOCIDAD COM
+  const int bRate = 115200;
+  const char sep = '|';
 
-  char* portName = "COM4";
+  // Getting available ports
+  char *list = strcat(list_ports(), "|"); // Add pipe for examples
 
-  int retval = open_port(portName, 115200);
-  if ( retval == TBK_OK ){
+  int x, count = 0;
+  for (x = 0; x <= strlen(list); x++)
+  {
+    if (list[x] == sep)
+    {
+      count++;
+    }
+  }
 
+  char **ports = malloc(count * sizeof(char *));
+  char *w;
+  int i, l, f = 0;
+
+  printf("Available ports : \n=================\n");
+  for (x = 0; x <= strlen(list); x++)
+  {
+    if (list[x] == sep)
+    {
+      w = malloc(l * sizeof(char *));
+      strncpy(w, list + i, l);
+      w[l] = 0;
+
+      printf("%i) %s\n", (f + 1), w); // Display option
+
+      // Store port (w) in array list
+      ports[f] = malloc(strlen(w) * sizeof(char *));
+      strcpy(ports[f], w);
+
+      f++;
+      i = x + 1; // Set new position
+      l = 0;
+      continue;
+    }
+
+    l++;
+  }
+
+  // Handle input option
+  printf("Please insert port option:");
+  int op;
+  scanf("%i", &op);
+
+  char *portName = ports[op - 1];
+  printf("Using option: %i -> \"%s\" port ...\n=================\n", op, portName);
+
+  int retval = open_port(portName, bRate);
+  if (retval == TBK_OK)
+  {
     printf("New Sale transaction...\n");
-    char * saleResponse = sale( 2500, 2, false);
+
+    char *saleResponse = sale(2500, 2, false); // sale(amount, ticket_number, send_messages)
     printf("Sale response %s\n", saleResponse);
-  } else{
-      printf("Unable to open selected port\n");
+  }
+  else
+  {
+    printf("Unable to open selected port\n");
   }
 
   //Close Port
   retval = close_port();
-  if(retval == SP_OK){
+  if (retval == SP_OK)
+  {
     puts("Serial port closed.\n");
-  } else{
+  }
+  else
+  {
     puts("Unable to close serial port.\n");
   }
 
