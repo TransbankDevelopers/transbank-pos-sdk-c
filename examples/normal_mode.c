241ba7abb23f5599b6313e3e9ee891d7994e6261
Leonel Muñoz <leonel.munoz@continuum.cl>
2019-04-30 10:33:51 -0400

    Refactor

--- examples/normal_mode.c
+++ examples/normal_mode.c
@@ -1,59 +1,10 @@
 #include "transbank.h"
 #include "transbank_serial_utils.h"
+#include "common.c"
 
 int main()
 {
-  // VELOCIDAD COM
-  const int bRate = 115200;
-  const char sep = '|';
-
-  // Getting available ports
-  char *list = strcat(list_ports(), "|"); // Add pipe for examples
-
-  int x, count = 0;
-  for (x = 0; x <= strlen(list); x++)
-  {
-    if (list[x] == sep)
-    {
-      count++;
-    }
-  }
-
-  char **ports = malloc(count * sizeof(char *));
-  char *w;
-  int i, l, f = 0;
-
-  printf("Available ports : \n=================\n");
-  for (x = 0; x <= strlen(list); x++)
-  {
-    if (list[x] == sep)
-    {
-      w = malloc(l * sizeof(char *));
-      strncpy(w, list + i, l);
-      w[l] = 0;
-
-      printf("%i) %s\n", (f + 1), w); // Display option
-
-      // Store port (w) in array list
-      ports[f] = malloc(strlen(w) * sizeof(char *));
-      strcpy(ports[f], w);
-
-      f++;
-      i = x + 1; // Set new position
-      l = 0;
-      continue;
-    }
-
-    l++;
-  }
-
-  // Handle input option
-  printf("Please insert port option:");
-  int op;
-  scanf("%i", &op);
-
-  char *portName = ports[op - 1];
-  printf("Using option: %i -> \"%s\" port ...\n=================\n", op, portName);
+  char *portName = select_port();
 
   int retval = open_port(portName, bRate);
   if (retval == TBK_OK)
