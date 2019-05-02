1c21c71d495af0608e39436caa787fd49d7c695d
Leonel Muñoz <leonel.munoz@continuum.cl>
2019-05-02 12:30:18 -0400

    Test file formatting

--- test/test_transbank.c
+++ test/test_transbank.c
@@ -4,55 +4,63 @@
 #include "transbank.h"
 #include <cmocka.h>
 
-int __wrap_write_message(struct sp_port *port, Message message){
+int __wrap_write_message(struct sp_port *port, Message message)
+{
     return mock();
 }
 
-int __wrap_read_ack(struct sp_port *port){
+int __wrap_read_ack(struct sp_port *port)
+{
     return mock();
 }
 
-int __wrap_read_bytes(struct sp_port *port, char* buf, Message message){
-    char response[] = { 0x02,
-                        0x30, 0x38, 0x31, 0x30, 0x07,
-                        0x30, 0x30, 0x07,
-                        0x35, 0x39, 0x37, 0x30, 0x32, 0x39, 0x34, 0x31, 0x34, 0x33, 0x30, 0x30, 0x07,
-                        0x37, 0x35, 0x30, 0x30, 0x31, 0x30, 0x38, 0x37, 0x03, 0x30, '\0'};
+int __wrap_read_bytes(struct sp_port *port, char *buf, Message message)
+{
+    char response[] = {0x02,
+                       0x30, 0x38, 0x31, 0x30, 0x07,
+                       0x30, 0x30, 0x07,
+                       0x35, 0x39, 0x37, 0x30, 0x32, 0x39, 0x34, 0x31, 0x34, 0x33, 0x30, 0x30, 0x07,
+                       0x37, 0x35, 0x30, 0x30, 0x31, 0x30, 0x38, 0x37, 0x03, 0x30, '\0'};
     strcpy(buf, response);
     return mock();
 }
 
-int __wrap_sp_input_waiting(struct sp_port *port){
+int __wrap_sp_input_waiting(struct sp_port *port)
+{
     return mock();
 }
 
-int __wrap_reply_ack(struct sp_port *port, char* message, int length){
+int __wrap_reply_ack(struct sp_port *port, char *message, int length)
+{
     return mock();
 }
 
-
-void test_pooling_ok(void **state){
-    (void) state; /* unused */
+void test_pooling_ok(void **state)
+{
+    (void)state; /* unused */
     will_return(__wrap_write_message, TBK_OK);
     will_return(__wrap_read_ack, TBK_OK);
     assert_int_equal((int)TBK_OK, polling());
 }
 
-void test_pooling_write_nok(void **state){
-    (void) state; /* unused */
+void test_pooling_write_nok(void **state)
+{
+    (void)state; /* unused */
     will_return_count(__wrap_write_message, TBK_NOK, 3);
     assert_int_equal((int)TBK_NOK, polling());
 }
 
-void test_pooling_ack_nok(void **state){
-    (void) state; /* unused */
+void test_pooling_ack_nok(void **state)
+{
+    (void)state; /* unused */
     will_return_count(__wrap_write_message, TBK_OK, 3);
     will_return_count(__wrap_read_ack, TBK_NOK, 3);
     assert_int_equal((int)TBK_NOK, polling());
 }
 
-void test_pooling_ok_on_second_try(void **state){
-    (void) state; /* unused */
+void test_pooling_ok_on_second_try(void **state)
+{
+    (void)state; /* unused */
     will_return(__wrap_write_message, TBK_NOK);
     will_return(__wrap_write_message, TBK_OK);
     will_return_count(__wrap_read_ack, TBK_OK, 1);
@@ -60,37 +68,42 @@ void test_pooling_ok_on_second_try(void **state){
     assert_int_equal((int)TBK_OK, polling());
 }
 
-void  test_pooling_ok_on_third_try(void **state){
-    (void) state; /* unused */
-    will_return_count(__wrap_write_message, TBK_NOK,2);
+void test_pooling_ok_on_third_try(void **state)
+{
+    (void)state; /* unused */
+    will_return_count(__wrap_write_message, TBK_NOK, 2);
     will_return(__wrap_write_message, TBK_OK);
     will_return_count(__wrap_read_ack, TBK_OK, 1);
 
     assert_int_equal((int)TBK_OK, polling());
 }
 
-void test_set_normal_mode_ok(void **state){
-    (void) state; /* unused */
+void test_set_normal_mode_ok(void **state)
+{
+    (void)state; /* unused */
     will_return(__wrap_write_message, TBK_OK);
     will_return(__wrap_read_ack, TBK_OK);
     assert_int_equal((int)TBK_OK, set_normal_mode());
 }
 
-void test_set_normal_mode_write_nok(void **state){
-    (void) state; /* unused */
+void test_set_normal_mode_write_nok(void **state)
+{
+    (void)state; /* unused */
     will_return_count(__wrap_write_message, TBK_NOK, 3);
     assert_int_equal((int)TBK_NOK, set_normal_mode());
 }
 
-void test_set_normal_mode_ack_nok(void **state){
-    (void) state; /* unused */
+void test_set_normal_mode_ack_nok(void **state)
+{
+    (void)state; /* unused */
     will_return_count(__wrap_write_message, TBK_OK, 3);
     will_return_count(__wrap_read_ack, TBK_NOK, 3);
     assert_int_equal((int)TBK_NOK, set_normal_mode());
 }
 
-void test_set_normal_mode_ok_on_second_try(void **state){
-    (void) state; /* unused */
+void test_set_normal_mode_ok_on_second_try(void **state)
+{
+    (void)state; /* unused */
     will_return(__wrap_write_message, TBK_NOK);
     will_return(__wrap_write_message, TBK_OK);
     will_return_count(__wrap_read_ack, TBK_OK, 1);
@@ -98,17 +111,19 @@ void test_set_normal_mode_ok_on_second_try(void **state){
     assert_int_equal((int)TBK_OK, set_normal_mode());
 }
 
-void test_set_normal_mode_ok_on_third_try(void **state){
-    (void) state; /* unused */
-    will_return_count(__wrap_write_message, TBK_NOK,2);
+void test_set_normal_mode_ok_on_third_try(void **state)
+{
+    (void)state; /* unused */
+    will_return_count(__wrap_write_message, TBK_NOK, 2);
     will_return(__wrap_write_message, TBK_OK);
     will_return_count(__wrap_read_ack, TBK_OK, 1);
 
     assert_int_equal((int)TBK_OK, set_normal_mode());
 }
 
-void test_load_keys_ok(void **state){
-    (void) state;
+void test_load_keys_ok(void **state)
+{
+    (void)state;
     will_return(__wrap_write_message, TBK_OK);
     will_return(__wrap_read_ack, TBK_OK);
     will_return(__wrap_read_bytes, 33);
@@ -119,30 +134,33 @@ void test_load_keys_ok(void **state){
 
     assert_int_equal(810, response.function);
     assert_int_equal(0, response.responseCode);
-    assert_int_equal(597029414300,response.commerceCode);
+    assert_int_equal(597029414300, response.commerceCode);
     assert_int_equal(75001087, response.terminalId);
     assert_int_equal(TBK_OK, response.initilized);
 }
 
-void test_load_keys_nok(void **state){
-    (void) state;
+void test_load_keys_nok(void **state)
+{
+    (void)state;
     will_return_count(__wrap_write_message, TBK_NOK, 3);
 
     BaseResponse response = load_keys();
     assert_int_equal(NULL, response.initilized);
 }
 
-void test_load_keys_ack_nok(void **state){
-    (void) state;
+void test_load_keys_ack_nok(void **state)
+{
+    (void)state;
     will_return_count(__wrap_write_message, TBK_OK, 3);
-    will_return_count(__wrap_read_ack, TBK_NOK,3);
+    will_return_count(__wrap_read_ack, TBK_NOK, 3);
 
     BaseResponse response = load_keys();
     assert_int_equal(NULL, response.initilized);
 }
 
-void test_load_keys_read_bytes_nok(void **state){
-    (void) state;
+void test_load_keys_read_bytes_nok(void **state)
+{
+    (void)state;
     will_return(__wrap_write_message, TBK_OK);
     will_return(__wrap_read_ack, TBK_OK);
     will_return_count(__wrap_read_bytes, -1, 3);
@@ -152,8 +170,9 @@ void test_load_keys_read_bytes_nok(void **state){
     assert_int_equal(NULL, response.initilized);
 }
 
-void test_load_keys_reply_ack_nok(void **state){
-    (void) state;
+void test_load_keys_reply_ack_nok(void **state)
+{
+    (void)state;
     will_return(__wrap_write_message, TBK_OK);
     will_return(__wrap_read_ack, TBK_OK);
     will_return_count(__wrap_read_bytes, 33, 3);
@@ -169,7 +188,7 @@ const struct CMUnitTest transbank_tests[] = {
     cmocka_unit_test(test_pooling_write_nok),
     cmocka_unit_test(test_pooling_ack_nok),
     cmocka_unit_test(test_pooling_ok_on_second_try),
-    cmocka_unit_test( test_pooling_ok_on_third_try),
+    cmocka_unit_test(test_pooling_ok_on_third_try),
     cmocka_unit_test(test_set_normal_mode_ok),
     cmocka_unit_test(test_set_normal_mode_write_nok),
     cmocka_unit_test(test_set_normal_mode_ack_nok),
@@ -179,8 +198,7 @@ const struct CMUnitTest transbank_tests[] = {
     cmocka_unit_test(test_load_keys_nok),
     cmocka_unit_test(test_load_keys_ack_nok),
     cmocka_unit_test(test_load_keys_read_bytes_nok),
-    cmocka_unit_test(test_load_keys_reply_ack_nok)
-};
+    cmocka_unit_test(test_load_keys_reply_ack_nok)};
 
 int main(void)
 {
